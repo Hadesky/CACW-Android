0348116a2a7ef4a164006b55aed925c77f7e72eb
Leaves <455173472@qq.com>
2015-10-28 21:25:27 +0800

    基本完善了数据库操作，测试了一部分功能，还有一些数据没有显示在View上,忘记写吧数据库close了，记得写！

--- app/src/main/java/com/hadesky/cacw/ui/ProjectDetailActivity.java
+++ app/src/main/java/com/hadesky/cacw/ui/ProjectDetailActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import com.hadesky.cacw.R;
 import com.hadesky.cacw.adapter.EditableMembersAdapter;
 import com.hadesky.cacw.bean.UserBean;
+import com.hadesky.cacw.database.DataBaseManager;
 import com.hadesky.cacw.tag.IntentTag;
 import com.hadesky.cacw.util.FullyGridLayoutManager;
 import com.hadesky.cacw.widget.StickView;
@@ -24,7 +25,7 @@ public class ProjectDetailActivity extends BaseActivity {
     private StickView doneTaskStick;
     private StickView undoTaskStick;
 
-    private DatabaseManager manager;
+    private DataBaseManager manager;
     private long projectId;
 
 
@@ -45,13 +46,13 @@ public class ProjectDetailActivity extends BaseActivity {
     }
 
     private void initData() {
-        manager = DatabaseManager.getInstance(context);
+        manager = DataBaseManager.getInstance(context);
         projectId = getIntent().getLongExtra(IntentTag.TAG_PROJECT_ID, 0);
 
-        DatabaseManager manager = DatabaseManager.getInstance(context);
+        DataBaseManager manager = DataBaseManager.getInstance(context);
 
         //查询操作
-        DatabaseManager.UserCursor userCursor = manager.queryUserFromProject(projectId);
+        DataBaseManager.UserCursor userCursor = manager.queryUserFromProject(projectId);
         userCursor.moveToFirst();
         members = new ArrayList<>();
         for (int i = 0; i < userCursor.getCount(); i++) {
