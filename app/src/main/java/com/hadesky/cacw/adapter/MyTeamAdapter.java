0089891eabc2f1d6549c2283524348391623a585
microstudent <455173472@qq.com>
2016-07-17 16:42:28 +0800

    修改MyTeam的样式，新增Palette颜色作为CardView背景

--- app/src/main/java/com/hadesky/cacw/adapter/MyTeamAdapter.java
+++ app/src/main/java/com/hadesky/cacw/adapter/MyTeamAdapter.java
@@ -2,14 +2,19 @@ package com.hadesky.cacw.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.annotation.LayoutRes;
-import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.hadesky.cacw.R;
 import com.hadesky.cacw.adapter.viewholder.BaseViewHolder;
 import com.hadesky.cacw.bean.TeamBean;
@@ -23,14 +28,13 @@ import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.datatype.BmobPointer;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
-import rx.Subscription;
 
 /**
  * 我的团队adapter
  * Created by 45517 on 2016/3/21.
  */
-public class MyTeamAdapter extends BaseAdapter<TeamMember>
-{
+public class MyTeamAdapter extends BaseAdapter<TeamMember> {
+
     public MyTeamAdapter(List<TeamMember> list, @LayoutRes int layoutid) {
         super(list, layoutid);
     }
@@ -41,7 +45,7 @@ public class MyTeamAdapter extends BaseAdapter<TeamMember>
         BaseViewHolder<TeamMember> holder = new BaseViewHolder<TeamMember>(v) {
             @Override
             public void setData(TeamMember teamBean) {
-                setTextView(R.id.tv_team_name,teamBean.getTeam().getTeamName());
+                setTextView(R.id.tv_team_name, teamBean.getTeam().getTeamName());
                 setTextView(R.id.tv_team_summary, teamBean.getTeam().getSummary());
 
                 BmobQuery<TeamMember> query = new BmobQuery<>();
@@ -49,16 +53,25 @@ public class MyTeamAdapter extends BaseAdapter<TeamMember>
                 query.findObjects(new FindListener<TeamMember>() {
                     @Override
                     public void done(List<TeamMember> list, BmobException e) {
-                        if (e==null) {
+                        if (e == null) {
                             View view = findView(R.id.layout_member_count);
                             view.setVisibility(View.VISIBLE);
                             setTextView(R.id.tv_member_count, "" + list.size());
                         }
                     }
                 });
-                ImageView view = findView(R.id.sdv_team_icon);
-                if (teamBean.getTeam().getTeamAvatar()!=null)
-                    view.setImageURI(Uri.parse(teamBean.getTeam().getTeamAvatar().getUrl()));
+                if (teamBean.getTeam().getTeamAvatar() != null) {
+                    SimpleDraweeView view = findView(R.id.sdv_team_icon);
+                    ImageRequest request = ImageRequestBuilder.newBuilderWithSource(Uri.parse(teamBean.getTeam().getTeamAvatar().getUrl()))
+                            .setPostprocessor(new PaletteProcessor((CardView) findView(R.id.card_view)))
+                            .build();
+
+                    DraweeController controller = Fresco.newDraweeControllerBuilder()
+                            .setImageRequest(request)
+                            .setOldController(view.getController())
+                            .build();
+                    view.setController(controller);
+                }
                 Button newProjectBt = findView(R.id.bt_new_project);
             }
         };
@@ -66,12 +79,33 @@ public class MyTeamAdapter extends BaseAdapter<TeamMember>
             @Override
             public void OnItemClick(View view, int position) {
                 TeamBean teamBean = mDatas.get(position).getTeam();
-                Intent intent=  new Intent(mContext, TeamInfoActivity.class);
+                Intent intent = new Intent(mContext, TeamInfoActivity.class);
                 intent.putExtra(TeamInfoActivity.IntentTag, teamBean);
                 mContext.startActivity(intent);
             }
         });
 
         return holder;
+    }
+
+    private void handleCardViewBg(CardView cardView, SimpleDraweeView simpleDraweeView) {
+        Bitmap bitmap = simpleDraweeView.getDrawingCache();
+        int bgColor = ImageUtils.getBitmapLightColor(bitmap);
+        cardView.setCardBackgroundColor(bgColor);
+    }
+
+    class PaletteProcessor extends BasePostprocessor {
+        private CardView mCardView;
+        public PaletteProcessor(CardView cardView) {
+            mCardView = cardView;
+        }
+
+        @Override
+        public void process(Bitmap bitmap) {
+            if (mCardView != null) {
+                int color = ImageUtils.getBitmapLightColor(bitmap);
+                mCardView.setCardBackgroundColor(color);
+            }
+        }
     }
 }
--- app/src/main/java/com/hadesky/cacw/util/ImageUtils.java
+++ app/src/main/java/com/hadesky/cacw/util/ImageUtils.java
@@ -1,8 +1,11 @@
 package com.hadesky.cacw.util;
 
 import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.graphics.Palette;
 
 /**
  * Created by MicroStudent on 2016/7/17.
@@ -15,9 +18,21 @@ public class ImageUtils {
         return wrapDrawable;
     }
 
-    public Drawable tintDrawable(Drawable drawable, ColorStateList colorStateList) {
+    public static Drawable tintDrawable(Drawable drawable, ColorStateList colorStateList) {
         final Drawable wrapDrawable = DrawableCompat.wrap(drawable);
         DrawableCompat.setTintList(wrapDrawable, colorStateList);
         return wrapDrawable;
     }
+
+    /**
+     * 依据一个bitmap获取主色调
+     * @param bitmap 要获取主色调的bitmap
+     * @return 主色调，或者默认白色
+     */
+    public static int getBitmapLightColor(Bitmap bitmap) {
+        Palette palette = new Palette.Builder(bitmap).generate();
+        return palette.getLightVibrantColor(Color.WHITE);
+    }
+
+
 }
