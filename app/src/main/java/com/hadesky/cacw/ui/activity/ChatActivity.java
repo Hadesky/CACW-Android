6d3a172dfc1d8b1725aa8c279e0490a28e8b2a54
dzysghr <dzysghr@live.com>
2016-07-23 14:46:42 +0800

    完成聊天界面基本布局

---
+++ app/src/main/java/com/hadesky/cacw/ui/activity/ChatActivity.java
@@ -0,0 +1,17 @@
+package com.hadesky.cacw.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hadesky.cacw.R;
+
+public class ChatActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
