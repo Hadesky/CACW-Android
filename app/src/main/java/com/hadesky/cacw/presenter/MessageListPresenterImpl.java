60fc0a29eb8bdd3a6b476830e083d26da7ca028e
dzysghr <dzysghr@live.com>
2016-07-23 12:54:45 +0800

    修复小问题

--- app/src/main/java/com/hadesky/cacw/presenter/MessageListPresenterImpl.java
+++ app/src/main/java/com/hadesky/cacw/presenter/MessageListPresenterImpl.java
@@ -16,7 +16,6 @@ import cn.bmob.v3.listener.FindListener;
 import rx.Subscription;
 
 /**
- *
  * Created by dzysg on 2016/7/23 0023.
  */
 public class MessageListPresenterImpl implements MessageListPresenter
@@ -40,7 +39,8 @@ public class MessageListPresenterImpl implements MessageListPresenter
     @Override
     public void onDestroy()
     {
-
+        if (mSubscription != null)
+            mSubscription.unsubscribe();
     }
 
     @Override
@@ -80,14 +80,14 @@ public class MessageListPresenterImpl implements MessageListPresenter
     {
         List<UserBean> users = mDatabaseManager.queryAllUsers();
         List<MessageBean> mlist = new ArrayList<>();
-        for(UserBean sb:users)
+        for(UserBean sb : users)
         {
-           List<MessageBean> templist =  mDatabaseManager.queryMessageByUser(sb.getObjectId(), 1, 0);//找出用户的第一条消息
-            if (templist.size()>0)
+            List<MessageBean> templist = mDatabaseManager.queryMessageByUser(sb.getObjectId(), 1, 0);//找出用户的第一条消息
+            if (templist.size() > 0)
             {
                 mlist.add(templist.get(0));
 
-            }else//如果不存在，就删除用户
+            } else//如果不存在，就删除用户
             {
                 mDatabaseManager.deleteUser(sb.getObjectId());
             }
@@ -99,7 +99,9 @@ public class MessageListPresenterImpl implements MessageListPresenter
     @Override
     public void deleteMessage(MessageBean bean)
     {
-        if (mSubscription != null)
-            mSubscription.unsubscribe();
+        if (MyApp.isCurrentUser(bean.getSender()))
+            mDatabaseManager.deleteUser(bean.getReceiver().getObjectId());
+        else
+            mDatabaseManager.deleteUser(bean.getSender().getObjectId());
     }
 }
